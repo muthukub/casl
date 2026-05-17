3aa325fc99af4173156bb449e385d42e8b5c6a14
Sergii <1159966+stalniy@users.noreply.github.com>
2026-05-17 22:43:16 +0200

    fix: expose PrismaAbility type

--- packages/casl-prisma/src/index.ts
+++ packages/casl-prisma/src/index.ts
@@ -1,4 +1,5 @@
 import type { Prisma } from '@prisma/client';
+import type { Abilities, Ability } from '@casl/ability';
 
 import type { PrismaModel, PrismaQueryFactory, PrismaTypes } from './runtime';
 
@@ -8,3 +9,5 @@ export type WhereInput<TModelName extends Prisma.ModelName> =
   PrismaTypes<Prisma.TypeMap>['WhereInput'][TModelName];
 export type PrismaQuery<T extends PrismaModel = PrismaModel> =
   PrismaQueryFactory<Prisma.TypeMap, T>;
+export type PrismaAbility<T extends Abilities, TConditions extends PrismaQuery = PrismaQuery> =
+  Ability<T, TConditions>;
